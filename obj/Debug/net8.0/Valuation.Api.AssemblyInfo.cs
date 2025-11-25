e81e56b869cc19af9546a21b0b218efde2fbd0b0
suryagodavarthi <suryagodavarthi13@gmail.com>
2025-11-25 14:00:14 +0530

    red flag

--- obj/Debug/net8.0/Valuation.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Valuation.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Valuation.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c9381c802abf7dcace48faf8a09eab2fc5c1d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b9314ade4cbf16dc3b5d2b0e62bac2c703daae6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Valuation.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Valuation.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
